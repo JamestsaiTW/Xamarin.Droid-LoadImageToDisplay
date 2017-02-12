ae020289778fbf3013d106ea9e8b03c778b62198
Jatsai <Jatsai@msn.com>
2017-02-13 00:16:22 +0800

    討論圖片可能根據本身所存在 App 內部可能的位置，對應產生的讀取機制

---
+++ XamarinDroidForLoadImageToDisplay/MainActivity.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Widget;
+using Android.OS;
+using Android.Runtime;
+
+namespace XamarinDroidForLoadImageToDisplay
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var mainImageView = FindViewById<ImageView>(Resource.Id.MainImageView);
+
+            var loadImageFromResourceButton = FindViewById<Button>(Resource.Id.LoadImageFromResourceButton);
+            loadImageFromResourceButton.Click += (sender, e) =>
+            {
+                mainImageView.SetImageResource(Resource.Drawable.Xamarin_Logo1);
+            };
+
+            var loadImageFromDrawableButton = FindViewById<Button>(Resource.Id.LoadImageFromDrawableButton);
+            loadImageFromDrawableButton.Click += (sender, e) =>
+            {
+                //此兩行都可以，選擇其中一行執行
+                //mainImageView.SetImageDrawable(GetDrawable(Resource.Drawable.Xamarin_Logo2));
+                mainImageView.SetImageDrawable(
+                    GetDrawable(Resources.GetIdentifier("xamarin_logo2", "drawable", PackageName)));
+            };
+
+            var loadImageFromAssertsButton = FindViewById<Button>(Resource.Id.LoadImageFromAssertsButton);
+            loadImageFromAssertsButton.Click += (sender, e) =>
+            {
+                //此兩行都可以，選擇其中一行執行
+                //mainImageView.SetImageDrawable(Android.Graphics.Drawables.Drawable.CreateFromStream(Assets.Open("Xamarin_Logo3.png"),null));
+                mainImageView.SetImageBitmap(BitmapFactory.DecodeStream(Assets.Open("Xamarin_Logo3.png")));
+            };
+
+            var loadImageFromLocalFileButton = FindViewById<Button>(Resource.Id.LoadImageFromLocalFileButton);
+            loadImageFromLocalFileButton.Click += async (sender, e) =>
+            {
+                var fileName = "Xamarin_Logo4.png";
+                var fullPath = System.IO.Path.Combine(FilesDir.Path, fileName);
+                if (!System.IO.File.Exists(fullPath))
+                {
+                    using (var stream = Assets.Open(fileName))
+                    {
+                        using (var file = System.IO.File.Create(fullPath))
+                        {
+                            await stream.CopyToAsync(file);
+                        }
+                    }
+                }
+                mainImageView.SetImageURI(Android.Net.Uri.Parse(fullPath));
+            };
+        }
+    }
+}
+
